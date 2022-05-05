6e7da8a877ea125b4b93fcea69055978fc7f58dd
Alexandra E <alexandra_elizarova@hotmail.com>
2022-05-05 16:35:58 +0300

    Added Page Object structure and rewrote the test

---
+++ pages/base_page.py
@@ -0,0 +1,8 @@
+# Page Object for the base page of the web site
+class BasePage(object):
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
+
+    def open(self):
+        self.browser.get(self.url)
